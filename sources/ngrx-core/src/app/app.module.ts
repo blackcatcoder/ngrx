bec2391f7674b6308b4760c5deddd8e532d0c285
blackcat <vinv.2491@hotmail.com>
2023-03-01 21:15:59 +0700

    update

--- sources/ngrx-core/src/app/app.module.ts
+++ sources/ngrx-core/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HumanComponent } from './human/human.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HumanComponent
   ],
   imports: [
     BrowserModule,
